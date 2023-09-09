1ead92e004c08cc13448e5387e366e19230f7032
DenisZaviryuhin <denzava30@gmail.com>
2023-09-09 21:20:18 +0300

    Initial commit

---
+++ src/app/Main01.java
@@ -0,0 +1,41 @@
+package app;
+//Протягом семи днів фіксувалась щоденна різна ціна товару.
+//За допомогою алгоритму Bubble sort реалізуйте функціонал,
+//який сортує значення цін в зростаючому порядку.
+//Також реалізуйте функціонал, який розраховує середню ціну
+//за весь період.
+public class Main01 {
+    static double sum;
+    private static String CURRENCY = "USD";
+
+    public static void main(String[] args) {
+        double[] prices = {2.99, 3.99, 1.99, 3.50, 4.29};
+        for (int i = 0; i < prices.length; i++) {
+            for (int b = 1; b < (prices.length - 1); b++) {
+                if (prices[b - 1] > prices[b]) {
+                    double temp = prices[b - 1];
+                    prices[b - 1] = prices[b];
+                    prices[b] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted prices in " + CURRENCY + " :" );
+        for (double price : prices) {
+            System.out.print(price + ", ");
+        }
+        sum = 0;
+        for (double price : prices) {
+            sum += price;}
+           double average = sum / prices.length;
+            System.out.printf("\nAverage price for all period : %.2f %s" ,average,CURRENCY);
+
+
+    }
+}
+
+
+
+
+
+
+
---
+++ src/app/Main02.java
@@ -0,0 +1,44 @@
+package app;
+
+public class Main02 {
+
+    private static String CURRENCY = "USD";
+    static double[] prices = {2.99, 3.99, 1.99, 3.50, 4.29};
+
+    public static void main(String[] args) {
+
+
+        getOutput(getAveragePrice(getSortedPrices(prices)));
+
+    }
+
+    private static double[] getSortedPrices(double[] prices) {
+        for (int i = 0; i < prices.length; i++) {
+            for (int b = 1; b < (prices.length - 1); b++) {
+                if (prices[b - 1] > prices[b]) {
+                    double temp = prices[b - 1];
+                    prices[b - 1] = prices[b];
+                    prices[b] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted prices in " + CURRENCY + " :");
+        for (double price : prices) {
+            System.out.print(price + " ");
+        }
+        return prices;
+    }
+
+    private static double getAveragePrice(double[] prices) {
+       double sum = 0.0;
+        for (double price1 : prices) {
+            sum += price1;
+        }
+        return sum / prices.length;
+    }
+
+    private static void getOutput(double average) {
+        System.out.printf("\nAverage price for all period : %.2f %s", average ,CURRENCY);
+
+    }
+}
